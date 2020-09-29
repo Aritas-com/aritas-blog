07ceaa64eb4b331282b643ec5a7d60e775af8c64
Andy Kunz <andy.kunz@aritas.com>
2020-09-29 21:22:25 +0200

    First Commit

---
+++ src/build/precache-manifest.ae92e01ed265099ceea73aa89a3b7f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a668e13b2bcab35d44a4e3450f1806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73d8a02db96b9b93dad",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "c111e6ab5ad091c19bc7",
+    "url": "/static/js/2.b1ace6ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b1ace6ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73d8a02db96b9b93dad",
+    "url": "/static/js/main.a083a5b2.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
